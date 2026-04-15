720ac9be28dfb1b7fde13e6462f62ea26742309e
Alex Turner <alexturner@meta.com>
2026-04-15 13:12:03 -0700

    Remove dead speculative_expansion.cpp/.h and compiler.cpp references
    The GuardType expansion now lives inside simplify.cpp's
    simplifyGuardType function. Remove the standalone pass files
    and their include/invocation in compiler.cpp.

--- cinderx/Jit/compiler.cpp
+++ cinderx/Jit/compiler.cpp
@@ -20,7 +20,6 @@
 #include "cinderx/Jit/hir/printer.h"
 #include "cinderx/Jit/hir/refcount_insertion.h"
 #include "cinderx/Jit/hir/simplify.h"
-#include "cinderx/Jit/hir/speculative_expansion.h"
 #include "cinderx/Jit/hir/ssa.h"
 #include "cinderx/Jit/jit_time_log.h"
 
@@ -89,7 +88,6 @@ void Compiler::runPasses(
   };
 
   runPassIf(hir::Simplify{}, PassConfig::kSimplify);
-  runPass(hir::SpeculativeExpansion{}, irfunc, callback);
   runPassIf(
       hir::DynamicComparisonElimination{}, PassConfig::kDynamicComparisonElim);
   runPassIf(hir::GuardTypeRemoval{}, PassConfig::kGuardTypeRemoval);
